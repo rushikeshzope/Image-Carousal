fb7f83479725489120ac127a04a785fed2ee9a84
rushikeshzope <rushikesh.zope@gmail.com>
2024-01-11 14:43:00 +0530

    Code Push

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import "./Carousel.css";
+import { images } from "../data/CarouselData";
+// you can explore more - and check as how to use materiul ui
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+// implement the class below
+class Carousel extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+        index:0
+        }
+    }
+    render(){
+        let {index}= this.state; //destructuring
+        let currentImage = images[index];
+        let handlePrev = () => {
+            this.setState({index:(this.state.index-1+images.length)%images.length})
+        }
+
+        let handleNext = () => {
+            this.setState({index:(this.state.index+1)%images.length})
+        }
+        return (
+            <>
+            <div className="mainContainer">
+                <div>
+                    <ArrowBackIosIcon onClick= {handlePrev}/>
+                </div>
+                <div className="imageBox">
+                  <img src={currentImage.img} alt="" />
+                </div>
+                <div className="imageContainer">
+                    <div className="titleBox">
+                <p >{currentImage.title}</p>
+                    </div>
+                    <div className="subTitleBox">
+                   <p>{currentImage.subtitle}</p>
+                    </div>
+
+                </div>
+                 <div>
+                   <ArrowForwardIosIcon onClick= {handleNext}/>
+                 </div>
+            </div>
+            </>
+        )
+    }
+ 
+  
+}
+
+export default Carousel;
